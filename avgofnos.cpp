532b5705d4d66db6fcb5c7672ed3ab78d458766c
Gagan <gagank2199@gmail.com>
2023-07-17 23:52:21 +0530

    avg of NOs

---
+++ avgofnos.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,k=0,n;
+    cin>>a;
+    for(int i=0;i<a;i++)
+    {
+        cin>>n;
+        k=k+n;
+    }
+    cout<< k/a;
+}
+
