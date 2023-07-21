9fb3513af3ab75a6db71848900fe22317f893687
Gagan <gagank2199@gmail.com>
2023-07-21 20:35:02 +0530

    armstrong number

--- armstrongnumber.cpp
+++ armstrongnumber.cpp
@@ -20,7 +20,7 @@
         if (result == num)
             cout << num << " is an Armstrong number. ";
         else
-            cout << num << " is not an Armstrong number. ";
+            cout << num << " is not an Armstrong number. "<<endl;
     
         return 0;
     }
