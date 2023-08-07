6883902b5ad4a8ee2ee6be799496007b7c252605
Gagan <gagank2199@gmail.com>
2023-08-07 11:21:55 +0530

    pattern *

--- new.cpp
+++ new.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 int main()
 {
-
+int n;
+cin>>n;
+int k=n;
+for(int i=1;i<=n;i++)
+{
+    for(int j=1;j<=n;j++)
+    {
+        if(j>=k)
+        cout<<"* ";
+        else
+        cout<<" ";
+    }
+    k--;
+    cout<<endl;
+}
 }
