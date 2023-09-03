b3bb3f6c720acea2ec95fda5c78f4a299213f15e
Gagan <gagank2199@gmail.com>
2023-09-03 19:07:42 +0530

    fibb seriese with n number

--- fibb.cpp
+++ fibb.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+
+int main()
+{
     int t1 = 0, t2 = 1, nextTerm = 0, n;
 
     cout << "Enter a positive number: ";
     cin >> n;
 
-
     cout << "Fibonacci Series: " << t1 << ", " << t2 << ", ";
 
     nextTerm = t1 + t2;
 
-    while(nextTerm <= n) {
+    while(nextTerm <= n)
+    {
         cout << nextTerm << ", ";
         t1 = t2;
         t2 = nextTerm;
