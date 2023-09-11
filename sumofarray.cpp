394cab1b7a62c2f5793c95742db947cac1702af7
Gagan <gagank2199@gmail.com>
2023-09-11 22:35:18 +0530

    sum arrayt

--- sumofarray.cpp
+++ sumofarray.cpp
@@ -22,6 +22,6 @@ int main()
 {
 	int arr[] = { 12, 3, 4, 15 ,23,43,13};
 	int n = sizeof(arr) / sizeof(arr[0]);
-	cout << "Sum of given array is " << sum(arr, n);
+	cout << "Sum of given array is = " << sum(arr, n);
 	return 0;
 }
