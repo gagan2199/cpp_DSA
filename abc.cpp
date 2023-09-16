a289b327625492c73dcb41c3f0765b0f1d45e503
Gagan <gagank2199@gmail.com>
2023-09-16 21:09:32 +0530

    abc

---
+++ abc.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+cin>>n;
+int k=n;
+for(int i=1;i<=n;i++)
+{
+    for(int j=1;j<=n;j++)
+    {
+        if(j>=k)
+        cout<<"* ";
+        else
+        cout<<" ";
+    }
+    
+    k--;
+    cout<< endl;
+}
+}
