b4468a04bcf3b334a6a17ae033e901641926a81d
Gagan Kumar <114229570+gagan2199@users.noreply.github.com>
2024-01-11 22:33:30 +0530

    Update avgofnos.cpp

--- avgofnos.cpp
+++ avgofnos.cpp
@@ -13,3 +13,21 @@ int main()
     cout<<"the avg is equal to"<< k/a;
 }
 
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,k=0,n;
+    cin>>a;
+    for(int i=0;i<a;i++)
+    {
+        cin>>n;
+        k=k+n;
+    }
+    cout<<"the avg is equal to"<< k/a;
+}
+
+*/
+
