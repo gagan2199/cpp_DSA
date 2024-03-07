e081552e65f3f66d34b0532b5603dff61cabe41a
Gagan Kumar <114229570+gagan2199@users.noreply.github.com>
2024-03-07 20:27:53 +0530

    Update armstrongnumber.cpp
    another method

--- armstrongnumber.cpp
+++ armstrongnumber.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-    using namespace std;
+    using namespace std;   
     
     int main() {
         int num, originalNum, remainder, result = 0;
@@ -13,7 +13,7 @@
             
             result += remainder * remainder * remainder;
             
-            // removing last digit from the orignal number
+            // removing last digit from the orignal number to get desired value
             originalNum /= 10;
         }
     
@@ -24,3 +24,31 @@
     
         return 0;
     }
+
+
+/* #include <iostream>
+    using namespace std;   
+    
+    int main() {
+        int num, originalNum, remainder, result = 0;
+        cout << "Enter a three-digit integer: ";
+        cin >> num;
+        originalNum = num;
+    
+        while (originalNum != 0) {
+            // remainder contains the last digit
+            remainder = originalNum % 10;
+            
+            result += remainder * remainder * remainder;
+            
+            // removing last digit from the orignal number to get desired value
+            originalNum /= 10;
+        }
+    
+        if (result == num)
+            cout << num << " is an Armstrong number. ";
+        else
+            cout << num << " is not an Armstrong number. "<<endl;
+    
+        return 0;
+    } */
